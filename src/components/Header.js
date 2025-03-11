9f63ecfd7ae4d37a30096941bf7d338cf8a83508
raja <marwah@MacBook-Air-de-Marwah.local>
2025-03-11 22:40:32 +0100

    mise à jour avec les compétences

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
           <ul>
             <li><a href="#about">À propos</a></li>
             <li><a href="#projects">Projets</a></li>
+            <li><a href="#skills">Compétences</a></li>
             <li><a href="#likes">Passions</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
