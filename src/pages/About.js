8aa130f8ef1ecaae0e13fcd3e16f728ce4415ce4
raja <marwah@MacBook-Air-de-Marwah.local>
2025-03-13 11:48:51 +0100

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,9 @@ const About = () => {
   return (
     <section id="about" className="about">
       <h2>À propos de moi</h2>
-      <p>Plongez dans l'univers de ma passion et de ma curiosité, où la technologie rencontre l'art et la cosmologie, esquissant le portrait d'une exploratrice infatigable du savoir et de l'innovation</p>
+      <p>Je me présente, Marwah, étudiante en Bachelor Concepteur Développeur d’Applications.
+        Je suis une exploratrice infatigable du savoir et de l’innovation. Passionnée par le développement informatique, je façonne des projets qui allient créativité et ingénierie. Toujours curieuse d’apprendre et de repousser mes limites, 
+        j’avance avec la conviction que chaque ligne de code peut dessiner un futur plus inspirant.</p>
     </section>
   );
 }
