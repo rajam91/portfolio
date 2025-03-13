87eb253a49ef09a7632cb9d39ec33484e0a186b7
raja <marwah@MacBook-Air-de-Marwah.local>
2025-03-13 11:45:22 +0100

    update

--- src/pages/Likes.js
+++ src/pages/Likes.js
@@ -9,9 +9,9 @@ const Likes = () => {
       description: "Passionnée par l’univers du développement, de l’IA et des nouvelles technologies.",
     },
     {
-      title: "Beauté & Maquillage",
+      title: "Beauté & Cosmétique",
       image: process.env.PUBLIC_URL + "/makeup.JPG",
-      description: "J’adore l’univers de la beauté et l’art du maquillage, un moyen d’expression infini.",
+      description: "J’adore l’univers de la beauté et du cosmétique un moyen d’expression infini.",
     },
     {
       title: "Cosmologie",
@@ -37,10 +37,10 @@ const Likes = () => {
       <p className="intro-text">
         D’un regard extérieur, mes passions pourraient sembler éparpillées aux quatre vents, 
         un éclectisme sans fil conducteur apparent. Pourtant, c’est précisément dans cette 
-        diversité que je me retrouve, que je me définis. Polyvalente, je navigue avec aisance 
-        entre l’informatique, le maquillage et la cosmologie, des univers qui, à première vue, 
+        diversité que je me définis. Polyvalente, je navigue avec aisance 
+        entre l’informatique, le cosmétique et la cosmologie, des univers qui, à première vue, 
         semblent n’avoir aucune corrélation. Mais, pour moi, ils dessinent ensemble le tableau 
-        complexe de mes intérêts, illustrant une soif d’apprendre insatiable.
+        de mes intérêts, illustrant une soif d’apprendre insatiable.
       </p>
 
       <div className="likes-container">
